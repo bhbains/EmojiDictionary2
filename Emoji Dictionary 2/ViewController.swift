5c518c6324a13916013156dfbc3416948700f15a
Brian <pepper@Edos-iMac.local>
2016-12-12 19:46:02 -0800

    Day 2

--- Emoji Dictionary 2/ViewController.swift
+++ Emoji Dictionary 2/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  @IBOutlet var daCoolTableView: UITableView!
+  
+  
+  
+  
 
   override func viewDidLoad() {
+    
+    
+    
     super.viewDidLoad()
+    
+    daCoolTableView.dataSource = self
+    
+    daCoolTableView.delegate = self
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
 
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    return 10
+    
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = UITableViewCell()
+    
+    cell.textLabel?.text = "EDO 😼"
+    
+    return cell
+    
+  }
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
